5f527546f86da008c3d63f079dbc1e0113f4fe71
Justin Wei <justinwei2010@ucla.edu>
2016-11-08 21:58:46 -0800

    Add in styling for detail page

--- app/src/main/java/com/example/android/popularmovies/task/RenderMovieDetailsTask.java
+++ app/src/main/java/com/example/android/popularmovies/task/RenderMovieDetailsTask.java
@@ -1,6 +1,7 @@
 package com.example.android.popularmovies.task;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +20,8 @@ import com.squareup.picasso.Picasso;
  */
 public class RenderMovieDetailsTask extends AsyncTask<String, Void, MovieDetailsModel> {
     private static final String LOG_TAG = RenderMovieDetailsTask.class.getSimpleName();
+    private static final double GOOD_RATING_THRESHOLD = 7.0;
+    private static final double BAD_RATING_THRESHOLD = 5.0;
 
     private Context mContext;
     private View mView;
@@ -40,18 +43,37 @@ public class RenderMovieDetailsTask extends AsyncTask<String, Void, MovieDetails
     protected void onPostExecute(final MovieDetailsModel model) {
         if (model != null) {
             ((TextView)mView.findViewById(R.id.movie_title)).setText(model.getTitle());
-            ((TextView)mView.findViewById(R.id.movie_user_rating)).setText(
-                    model.getUserRating());
             ((TextView)mView.findViewById(R.id.movie_release_date)).setText(
-                    model.getReleaseDate());
+                    getFormattedReleaseDate(model.getReleaseDate()));
             ((TextView)mView.findViewById(R.id.movie_overview)).setText(
                     model.getOverview());
 
+            //Set user ratings
+            final TextView userRatingView = ((TextView)mView.findViewById(R.id.movie_user_rating));
+            userRatingView.setText(model.getUserRating());
+            setColorForUserRatings(userRatingView);
+
+            //Set thumbnail picture
             final String posterURL = URLBuilder.getMoviePosterURL(model.getImagePath());
             Log.v(LOG_TAG, "weijusti posterURL: " + posterURL);
-            final ImageView imageView = (ImageView) mView.findViewById(
-                    R.id.movie_poster_thumbnail);
+            final ImageView imageView = (ImageView) mView.findViewById(R.id.movie_poster_thumbnail);
             Picasso.with(mContext).load(posterURL).into(imageView);
+        }
+    }
+
+    private String getFormattedReleaseDate(final String releaseDate) {
+        return releaseDate.replaceAll("-", ".");
+    }
+
+    private void setColorForUserRatings(final TextView textView) {
+        final double ratings = Double.parseDouble(textView.getText().toString());
+        final GradientDrawable bgShape = (GradientDrawable)textView.getBackground();
+        if (ratings <= GOOD_RATING_THRESHOLD && ratings > BAD_RATING_THRESHOLD) {
+            bgShape.setColor(mContext.getResources().getColor(R.color.colorDecentRating));
+        } else if (ratings <= BAD_RATING_THRESHOLD) {
+            bgShape.setColor(mContext.getResources().getColor(R.color.colorBadRating));
+        } else {
+            bgShape.setColor(mContext.getResources().getColor(R.color.colorGoodRating));
         }
     }
 }
